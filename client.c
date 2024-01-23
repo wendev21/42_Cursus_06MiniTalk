fe38365265b752932b22e9516cd5760bad8ed738
wendev21 <111527516+wendev21@users.noreply.github.com>
2024-01-23 17:01:26 +0100

    first commit

--- client.c
+++ client.c
@@ -6,21 +6,58 @@
 /*   By: wecorzo- <wecorzo-@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/22 10:24:31 by wecorzo-          #+#    #+#             */
-/*   Updated: 2024/01/22 16:27:03 by wecorzo-         ###   ########.fr       */
+/*   Updated: 2024/01/23 15:49:36 by wecorzo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./Libft/libft.h"
 #include <signal.h>
+#include <stdio.h>
+
+void	send_binario(char *str, int pid)
+{
+	int	i;
+	char	c;
+
+	while (*str)
+	{
+		c = *str;
+		i = 7;
+		while (i >= 0)
+		{
+			if (c & (1 << i))
+				kill(pid, SIGUSR1);
+			else
+				kill(pid, SIGUSR2);
+			i--;
+			usleep(380);
+		}
+		str++;
+	}
+	/*
+	if (num > 1)
+		send_binario(num / 2, pid);
+	if  (num % 2 == 1)
+	{
+		kill(pid, SIGUSR1);
+		usleep(350);
+	}
+	else if (num % 2 == 0)
+	{
+		kill(pid, SIGUSR2);
+		usleep(350);
+	}*/
+}
 
 int main(int argc, char **argv)
 {
 	pid_t	pid;
+	char	*str;
 
 	if (argc != 3)
 		return (1);
 	pid = ft_atoi(argv[1]);
-	ft_putstr_fd(argv[1], 1);
-	kill(pid, SIGUSR1);
+	str = argv[2];
+	send_binario(str, pid);
 	return (0);
 }
--- server.c
+++ server.c
@@ -6,28 +6,47 @@
 /*   By: wecorzo- <wecorzo-@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/22 12:19:52 by wecorzo-          #+#    #+#             */
-/*   Updated: 2024/01/22 15:22:49 by wecorzo-         ###   ########.fr       */
+/*   Updated: 2024/01/23 16:58:11 by wecorzo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./Libft/libft.h"
 #include <signal.h>
+#include <stdio.h>
 
 void handler(int sign)
 {
-	(void)sign;
-	write(1, "recived \n", 9);
+	static char	result;
+	static int	i;
+
+	if(sign == SIGUSR1)
+	{
+		result <<= 1;
+		result+= 1;
+	}
+	else
+	{
+		result <<= 1;
+		result+= 0;
+	}
+	i++;
+	if (i == 8)
+	{
+		write(1, &result, 1);
+		result = 0;
+		i = 0;
+	}
+
 }
 int main(void)
 {
-	char	*str;
 	pid_t	pid;
 
-	if (signal(SIGUSR1, handler) == SIG_ERR)
+	if ((signal(SIGUSR1, handler) == SIG_ERR) || signal(SIGUSR2, handler) == SIG_ERR)
 		write(2, "Error Al configurar el Manejador de Señales \n", 44);
 	pid = getpid();
-	str = ft_itoa(pid);
-	ft_putstr_fd(str, 1);
+	ft_putstr_fd(ft_itoa(pid), 1);
+	write(1, "\n", 1);
 	while (1)
 		sleep(1);
 	return (0);
